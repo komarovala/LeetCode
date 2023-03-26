68f8060417e011f31c075dfef65e01371b1654ca
komarovala <starkcom8@mail.ru>
2023-03-26 20:22:29 +0300

    re-solve PlusOne.py

--- Easy/PlusOne.py
+++ Easy/PlusOne.py
@@ -1,8 +1,8 @@
-#https://leetcode.com/problems/plus-one/
-#O(n^2)
+# https://leetcode.com/problems/plus-one/
+# O(n^2)
 from typing import List
 
-digits = [4, 3, 2, 1]
+digits = [4,3,2,1]
 
 
 def plusOne(digits) -> List[int]:
@@ -17,4 +17,29 @@ def plusOne(digits) -> List[int]:
 
     return n
 
-plusOne(digits)
+
+# plusOne(digits)
+
+# Fair solution
+
+
+def plusOne(digits: List[int]) -> List[int]:
+    last_num = len(digits) - 1
+    in_mind = 1
+
+    while last_num > 0:
+        digits[last_num] = digits[last_num] + in_mind
+
+        if digits[last_num] == 10:
+            digits[last_num] = 0
+            in_mind = 1
+            last_num -= 1
+        else:
+            return digits
+
+    if digits[0] + in_mind == 10:
+        return ([1, 0] + digits[1:])
+    else:
+        digits[0] += in_mind
+
+    return digits
