cc75da1a6746713d30cbb650c12675541607ea60
komarovala <starkcom8@mail.ru>
2023-05-01 18:21:21 +0300

    add Search in Rotated Sorted Array.py

---
+++ Medium/Search in Rotated Sorted Array.py
@@ -0,0 +1,53 @@
+# https://leetcode.com/problems/search-in-rotated-sorted-array/
+nums = [5,1,2,3,4]
+target = 5
+
+
+def search(nums, target) -> int:
+    def find_rotated_num(nums):
+        l = 0
+        r = len(nums) - 1
+        while l + 1 < r:
+            median = (l + r) // 2
+            if nums[l] > nums[median]:
+                r = median
+            else:
+                l = median
+
+        if nums[l] > nums[r]:
+            return l
+        else:
+            return r
+
+    if len(nums) == 1 and nums[0] == target:
+        return 0
+    elif len(nums) == 1 and nums[0] != target:
+        return -1
+    else:
+        rot_index = find_rotated_num(nums)
+        if (nums[rot_index] >= target) and (nums[0] <= target):
+            start = 0
+            end = rot_index
+        else:
+            start = rot_index + 1
+            end = len(nums) - 1
+
+        while start <= end:
+            if (start == end) and (nums[start] == target):
+                return start
+            elif (start == end) and (nums[start] != target):
+                return -1
+            else:
+                median = (start + end) // 2
+
+                if nums[median] == target:
+                    return (median)
+                elif nums[median] > target:
+                    end -= 1
+                else:
+                    start += 1
+    return -1
+
+
+
+
